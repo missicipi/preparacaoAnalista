98a44779f12012b37ff0b88af64001907fa646fb
missicipi <professorulissesm@gmail.com>
2020-07-09 23:01:34 -0300

    Banco e Exception personalizada

---
+++ src/Application/Program.java
@@ -0,0 +1,17 @@
+package Application;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
+
+	}
+
+}
